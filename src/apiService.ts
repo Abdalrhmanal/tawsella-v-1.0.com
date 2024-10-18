9af0db0f347f7f24ab045f9abea37be5e6bad8ba
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2024-10-18 22:34:32 +0300

    Fix error becose URL API in servise

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,18 +1,10 @@
 /* import axios from 'axios';
 
-<<<<<<< HEAD
 const API_BASE_URL = 'https://tawsella.online/api';
  */
 // apiService.ts
 export async function fetchData(endpoint: string): Promise<any> {
     const response = await fetch(`https://tawsella.online/api${endpoint}`);
-=======
-const API_BASE_URL = 'http://https://tawsella.online/api';
- */
-// apiService.ts
-export async function fetchData(endpoint: string): Promise<any> {
-    const response = await fetch(`http://https://tawsella.online/api${endpoint}`);
->>>>>>> 8ece63a93a954746e9a60c70c5bace5436cdf940
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
