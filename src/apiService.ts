7aaef6ebb0336629c1c1ccc1e6206fade2e84d3b
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2024-10-18 22:00:10 +0300

    fix Api url 2

--- src/apiService.ts
+++ src/apiService.ts
@@ -7,11 +7,11 @@ const API_BASE_URL = 'https://tawsella.online/api';
 export async function fetchData(endpoint: string): Promise<any> {
     const response = await fetch(`https://tawsella.online/api${endpoint}`);
 =======
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'http://https://tawsella.online/api';
  */
 // apiService.ts
 export async function fetchData(endpoint: string): Promise<any> {
-    const response = await fetch(`http://127.0.0.1:8000/api${endpoint}`);
+    const response = await fetch(`http://https://tawsella.online/api${endpoint}`);
 >>>>>>> 8ece63a93a954746e9a60c70c5bace5436cdf940
     if (!response.ok) {
       throw new Error('Network response was not ok');
